8eab34fde9ddde35c31248a3ec1cf925a3690982
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-01-14 23:02:33 +0000

    Formatting and fixing docs

--- src/Utils/Utilities.java
+++ src/Utils/Utilities.java
@@ -6,20 +6,14 @@ import java.util.regex.*;
 public interface Utilities {
 
     /**
-     * Method to check the number of '@' on a string
+     * Method to validate an email using regex
      * @param email String to check
      * @return True if the email is valid, false otherwise
      */
     static boolean checkEmail(String email){
         Pattern p = Pattern.compile("^\\w+([\\.-]?\\w+)*@\\w+(\\.\\w{2,3})+$");//. represents single character
         Matcher m = p.matcher(email);
-        boolean b = m.matches();
-        /*char[] chars = email.toCharArray();
-        int ac = 0;
-        for(char c : chars)
-            if(c == '@') ac++;
-        return (ac == 1);*/
-        return b;
+        return m.matches();
     }
 
 }
